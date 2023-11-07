bec055d21659bea3218d05f9002644d2dcfa6957
Gapz@ThaiLand <32583026+apiwat59@users.noreply.github.com>
2023-11-07 21:01:30 +0700

    Update portfolioData.js

--- src/components/portfolio/portfolioData.js
+++ src/components/portfolio/portfolioData.js
@@ -1,5 +1,5 @@
 import Image1 from "../../../public/assets/img/portfolio/projector.png";
-
+import Image2 from "../../../public/assets/img/portfolio/project-2.jpg";
 
 const PortfolioData = [
   {
@@ -15,6 +15,22 @@ const PortfolioData = [
         language: "HTML, CSS, Javascript",
         preview: "projector.gazdev.cloud",
         link: "https://projector.gazdev.cloud/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: "OBS",
+    image: Image2,
+    tag: ["Audiovisual"],
+    delayAnimation: "100",
+    modalDetails: [
+      {
+        project: "OBS Remote Control",
+        client: "Me",
+        language: "Svelte, CSS, Javascript",
+        preview: "gazdev.cloud/obs-web",
+        link: "https://gazdev.cloud/obs-web",
       },
     ],
   }
